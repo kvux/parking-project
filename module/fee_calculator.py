5ded0ac5e0ed864976e29748585093377ed89928
kvux <125789389+kvux@users.noreply.github.com>
2026-03-19 14:36:57 +0900

    Add fee_calculator module with fee calculation function
    Implement fee calculation function for parking.

---
+++ module/fee_calculator.py
@@ -0,0 +1,12 @@
+from datetime import date time
+import math
+
+PARKING_RATE_PER_HOUR = 10000
+
+def calculate_fee(entry_time, exit_time=None);
+"""
+Calculate parking fee based on duration
+
+Args:
+entry_time: datetime object
+exit_time: dateime object (default to now)
